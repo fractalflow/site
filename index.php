832ab8757de2b392206ecc9721675d101af4f3f0
fractalflow <144395354+fractalflow@users.noreply.github.com>
2023-09-07 20:09:30 -0300

    adicionei arquivo index

---
+++ index.php
@@ -0,0 +1,284 @@
+<html>
+<head>
+<title>Turmas do Professor Perera</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000" leftmargin="1" topmargin="5" marginwidth="0" marginheight="0">
+<table width="778" border="0" cellspacing="0" cellpadding="0" align="center">
+  <tr>
+    <td align="center" valign="top" width="154">
+      <table width="154" border="0" cellspacing="0" cellpadding="0">
+        <tr> 
+          <td bgcolor="#000000" valign="middle" align="center"> 
+            <table width="152" border="0" cellspacing="0" cellpadding="1">
+              <tr> 
+                <td bgcolor="#000000" height="1"></td>
+              </tr>
+              <tr> 
+                <td bgcolor="#990000"><font color="#FFFFFF" size="2" face="Tahoma"><b>:: 
+                  Minha Carreira</b></font></td>
+              </tr>
+              <tr> 
+                <td bgcolor="#000000" height="1"></td>
+              </tr>
+              <tr> 
+                <td bgcolor="#FFCCCC"> 
+                  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr> 
+                      <td width="8"><font size="2" face="Tahoma">&nbsp;</font></td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../mc/minhacarreira.php" target="mainFrame">Carreiras</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td width="8"><font size="2" face="Tahoma">&nbsp;</font></td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../mc/passos.1.htm" target="mainFrame">Est&aacute;gio</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td width="8"><font size="2" face="Tahoma">&nbsp;</font></td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../mc/passos.2.htm" target="mainFrame">Interc&acirc;mbio</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td width="8"><font size="2" face="Tahoma">&nbsp;</font></td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../mc/mba.htm" target="mainFrame">P&oacute;s-Gradua&ccedil;&atilde;o 
+                        e MBA</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td width="8"><font size="2" face="Tahoma">&nbsp;</font></td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../mc/museus.htm" target="mainFrame">Cultura 
+                        e Lazer</a></font></td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr> 
+                <td bgcolor="#000000" height="1"></td>
+              </tr>
+              <tr> 
+                <td bgcolor="#FF9933"><font face="Tahoma"><b><font size="2" color="#FFFFFF">:: 
+                  Professores</font></b></font></td>
+              </tr>
+              <tr> 
+                <td bgcolor="#000000" height="1"></td>
+              </tr>
+              <tr> 
+                <td bgcolor="#FFCC99"> 
+                  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr> 
+                      <td width="8"><font size="2" face="Tahoma">&nbsp;</font></td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../professores.php" target="mainFrame">Material 
+                        de Apoio</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td width="8"><font size="2" face="Tahoma">&nbsp;</font></td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../cadastro.php" target="mainFrame">Cadastro</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td width="8"><font size="2" face="Tahoma">&nbsp;</font></td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../ajuda/index.htm" target="mainFrame">F.A.Q.</a></font></td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr> 
+                <td bgcolor="#000000" height="1"></td>
+              </tr>
+              <tr> 
+                <td bgcolor="#0033CC"><font color="#FFFFFF"><b><font size="2" face="Tahoma">:: 
+                  Cursos</font></b></font></td>
+              </tr>
+              <tr> 
+                <td bgcolor="#000000" height="1"></td>
+              </tr>
+              <tr> 
+                <td bgcolor="#99CCFF"> 
+                  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr> 
+                      <td width="8"><font size="2" face="Tahoma">&nbsp;</font></td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../cursos/indice.php" target="mainFrame">Cat&aacute;logo</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td width="8">&nbsp;</td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../cursos/andamento.htm" target="mainFrame">Cursos 
+                        em Andamento</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td width="8">&nbsp;</td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../cursoonline/curso_online.php" target="mainFrame">Cursos 
+                        Online</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td width="8"><font size="2" face="Tahoma">&nbsp;</font></td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../cursos/parceiros.htm" target="mainFrame">Parceiros</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td width="8"><font size="2" face="Tahoma">&nbsp;</font></td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../cursos/preview.htm" target="mainFrame">Preview 
+                        de Cursos</a></font></td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr> 
+                <td bgcolor="#000000"></td>
+              </tr>
+              <tr> 
+                <td bgcolor="#009900"><font color="#FFFFFF"><b><font size="2" face="Tahoma">:: 
+                  Financial Adviser</font></b></font></td>
+              </tr>
+              <tr> 
+                <td bgcolor="#000000" height="1"></td>
+              </tr>
+              <tr> 
+                <td bgcolor="#CCFFCC"> 
+                  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr> 
+                      <td width="8"><font size="2" face="Tahoma">&nbsp;</font></td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../financial/not%EDcias.php" target="mainFrame">Not&iacute;cias</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td width="8"><font size="2" face="Tahoma">&nbsp;</font></td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../financial/dic_a_l.htm">Dicion&aacute;rio Financeiro</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td width="8">&nbsp;</td>
+                      <td width="*"><font face="Tahoma" size="2" color="#000000">&raquo; 
+                        <a href="../fin_adv.htm">Textos Interessantes</a></font></td>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+              <tr> 
+                <td bgcolor="#000000" height="1"></td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+      <br>
+      
+      <table width="100%" border="0" cellspacing="0" cellpadding="2">
+        <tr>
+          <td valign="middle" align="center">&nbsp;</td>
+        </tr>
+      </table>
+      </td>
+    <td valign="top" width="476" bgcolor="#FFFFFF">
+      <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr> 
+          <td background="../images/title2.gif" height="27" align="right"><font face="Tahoma" size="3" color="#FF9933"><b>Luiz
+            Carlos Jacob Perera&nbsp;&nbsp;&nbsp;&nbsp;</b></font></td>
+        </tr>
+        <tr> 
+          <td> 
+            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr> 
+                <td width="5">&nbsp;</td>
+                <td width="*"> 
+                  <p><font face="Tahoma" size="2" color="#FF9933"> &nbsp;Professores
+                    &raquo; Luiz Carlos Jacob Perera</font></p>
+                   
+                  <p align="left">&nbsp; </p>
+                  <p align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">Prezados 
+                    Alunos:</font></b></p>
+                  <p align="left"><font size="2" face="Arial, Helvetica, sans-serif">Aqui 
+                    est&atilde;o a disposi&ccedil;&atilde;o o material de apoio.</font></p>
+                  <p align="left"><font size="2" face="Arial, Helvetica, sans-serif">Escolha 
+                    a sua turma e coloque a sua senha.</font></p>
+                  <p align="left"><br>
+                  </p>
+                  <table width="339" border="0" align="center">
+                    <tr> 
+                      <td width="333"><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><a href="6w">Turma 
+                        -- 6W</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><a href="7w">Turma 
+                        -- 7W</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><a href="lato">Turma 
+                        -- Mackenzie Lato Sensu </a></font></td>
+                    </tr>
+                    <tr> 
+                      <td><font size="2" face="Verdana, Arial, Helvetica, sans-serif"><a href="facef">Turma 
+                        -- P&oacute;s-Gradua&ccedil;&atilde;o -- FACEF</a></font></td>
+                    </tr>
+                    <tr> 
+                      <td>&nbsp;</td>
+                    </tr>
+                  </table>
+                  <p align="left">&nbsp; </p>
+                  
+                  <p align="right"><font face="Tahoma" size="2" color="#FF9933"></font> 
+                  </p>
+                </td>
+                <td width="5">&nbsp;</td>
+              </tr>
+            </table>
+            
+          </td>
+        </tr>
+        <tr> 
+          <td></td>
+        </tr>
+        <tr>
+          <td>
+            <div align="center"> 
+              <p><font size="2" face="Tahoma"><a href="http://www.minhacarreira.com.br/home.php">Home</a> 
+                | <a href="../ajuda/index.htm" target="mainFrame">Ajuda</a> | 
+                <a href="../contato.htm" target="mainFrame">Contato</a> | Mapa 
+                do Site<br>
+                <a href="../mc/minhacarreira.php" target="mainFrame">Minha Carreira</a> 
+                | <a href="../professores.php" target="mainFrame">Professores</a> 
+                | <a href="../cursos/cursos.php" target="mainFrame">Cursos</a> 
+                | <a href="../fin_adv.htm" target="mainFrame">Financial Adviser</a></font><font size="1" face="Arial"></font><font size="1" face="Arial"><br>
+                <br>
+                Copyright &copy; Ascent Educa&ccedil;&atilde;o e Treinamento. 
+                Todos os direitos reservados.<br>
+                e-mail:<a href="mailto:info@minhacarreira.com.br"> info@minhacarreira.com.br 
+                </a></font></p>
+              </div>
+          </td>
+        </tr>
+      </table>
+    </td>
+    <td align="center" valign="top" width="148" bgcolor="#FFFFFF">
+      
+      <table width="100%" border="0" cellspacing="0" cellpadding="2">
+        <tr>
+          <td>&nbsp;</td>
+        </tr>
+      </table>
+      
+      <table width="100%" border="0" cellspacing="0" cellpadding="2">
+        <tr>
+          <td>&nbsp;</td>
+        </tr>
+      </table>
+      
+      <table width="100%" border="0" cellspacing="0" cellpadding="2">
+        <tr>
+          <td valign="middle" align="center">&nbsp;</td>
+        </tr>
+      </table>
+      
+    </td>
+  </tr>
+</table>
+</body>
+</html>
